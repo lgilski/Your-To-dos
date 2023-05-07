9c39cc3f6ba81d32ae7e94fd16a5893ea0da93f7
COUGAR <COUGAR@gmail.com>
2023-05-07 18:54:18 +0200

    Another animations improvement

--- src/components/Plans.js
+++ src/components/Plans.js
@@ -3,6 +3,8 @@ import classes from './Plans.module.css';
 
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
+import cardClasses from './Card.module.css';
+
 const Plans = function (props) {
   const cards = props.plans.map(plan => {
     const date = new Date(plan.date);
@@ -12,7 +14,16 @@ const Plans = function (props) {
     const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
     return (
-      <CSSTransition key={plan.id} classNames='fade' timeout={300}>
+      <CSSTransition
+        key={plan.id}
+        classNames={{
+          enterActive: cardClasses['fade-enter-active'],
+          enter: cardClasses['fade-enter'],
+          exitActive: cardClasses['fade-exit-active'],
+          exit: cardClasses['fade-exit'],
+        }}
+        timeout={300}
+      >
         <Card
           key={plan.id}
           onDelete={props.onDelete}
@@ -35,7 +46,15 @@ const Plans = function (props) {
     <div className={classes.plansContainer}>
       <TransitionGroup>
         {props.plans.length === 0 && (
-          <CSSTransition classNames='fade' timeout={300}>
+          <CSSTransition
+            classNames={{
+              enterActive: classes['message-enter-active'],
+              enter: classes['message-enter'],
+              exitActive: classes['message-exit-active'],
+              exit: classes['message-exit'],
+            }}
+            timeout={300}
+          >
             <h4 className={classes.message}>There are no plans yet</h4>
           </CSSTransition>
         )}
