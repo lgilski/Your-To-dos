d237904755965e31c583efa621d1bd2757a522ce
COUGAR <COUGAR@gmail.com>
2023-07-17 16:36:11 +0200

    small twicks

--- src/components/Home/Subtitle/Subtitle.tsx
+++ src/components/Home/Subtitle/Subtitle.tsx
@@ -6,7 +6,7 @@ const Subtitle = function ({ type }: { type?: string }) {
   const user = auth.currentUser;
 
   return (
-    <div className={'flex flex-col gap-2 items-center justify-items-center '}>
+    <div className={'flex flex-col gap-2 items-center'}>
       <Link
         to={!user ? '/' : '/app/cards'}
         className={clsx(
