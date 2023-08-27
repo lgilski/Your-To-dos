7c839c9da11c72ccd6ecdbe5e883df7f57987c77
COUGAR <COUGAR@gmail.com>
2023-08-27 20:04:32 +0200

    to the previous

--- src/components/Community/views/FriendsView/AddFriends.tsx
+++ src/components/Community/views/FriendsView/AddFriends.tsx
@@ -81,20 +81,20 @@ function AddFriends() {
         <form onSubmit={addFriend} className='flex flex-col'>
           <h5 className='text-2xl'>Add friend</h5>
           <label htmlFor='addFriend' className='mb-3 text-base'>
-            Type your friend&apos;s user name to sent request
+            Type your friend&apos;s user name to send request
           </label>
           <div className='bg-orange-050 dark:bg-grey-900 px-2 py-3 rounded-lg flex justify-between w-full'>
             <input
               className='bg-inherit border-none w-full text-lg px-1 py-1 focus:outline-none dark:text-grey-100'
               maxLength={32}
-              placeholder="Type your friend's user name to sent request"
+              placeholder="Type your friend's user name to send request"
               type='text'
               name='addFriend'
               autoComplete='off'
             />
             <button
               type='submit'
-              className='mx-2 text-xl whitespace-nowrap bg-lime-green-200 border-none rounded hover:bg-lime-green-300 font-medium text-lime-green-800 py-1 px-2 cursor-pointer'
+              className='mx-2 text-xl whitespace-nowrap bg-lime-green-200 border-none rounded hover:bg-lime-green-300 font-medium text-lime-green-800 py-1 px-2 cursor-pointer duration-300'
             >
               Send friend request
             </button>
